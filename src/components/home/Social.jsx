374e1366d9fef72997c8948cbb7b7f0a5597ab0e
Jerome Decinco <57111980+CodeKaito@users.noreply.github.com>
2022-12-22 11:10:25 +0100

    Add files via upload

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable react/jsx-no-target-blank */
+import React from 'react';
+
+const Social = () => {
+  return (
+    <div className='home__social'>
+        <a href='https://www.instagram.com/carljeromee/' className='home__social-icon' target='_blank'>
+            <i class='uil uil-instagram'></i>
+        </a>
+
+        <a href='https://www.facebook.com/jeromeecarl' className='home__social-icon' target='_blank'>
+            <i class='uil uil-facebook'></i>
+        </a>
+
+        <a href='https://www.linkedin.com/in/jerome-de-cinco-4b4904189/' className='home__social-icon' target='_blank'>
+            <i class='uil uil-linkedin-alt'></i>
+        </a>
+
+    </div>
+  )
+}
+
+export default Social
---
+++ src/components/testimonials/Data.jsx
@@ -0,0 +1,35 @@
+import Image1 from '../../assets/assets/testimonial1.png';
+import Image2 from '../../assets/assets/testimonial2.png';
+import Image3 from '../../assets/assets/testimonial3.png';
+import Image4 from '../../assets/assets/testimonial4.png';
+
+export const Data = [
+    {
+        id: 4,
+        image: Image4,
+        title: 'Steve Becker | Senior Software Engineer',
+        description: 
+            'Jerome He was always more successful as the project manager in meeting deadlines and finishing tasks.',
+    },
+    {
+        id: 1,
+        image: Image1,
+        title: 'Harry Lee | Software Developer',
+        description: 
+            'Jerome did really good job, all aspects of the project were follower step by step and with good results and performances.',
+    },
+    {
+        id: 2,
+        image: Image2,
+        title: 'Sophie Clinton | Frontend Developer',
+        description:
+            'Jerome has extensive knowledge in all aspects of software engineering and development.',
+    },
+    {
+        id: 3,
+        image: Image3,
+        title: 'Sara Cill | Software Engineer',
+        description:
+            'John sets an excellent example for others by pushing them to develop in their areas.',
+    }
+]
