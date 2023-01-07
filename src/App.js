87a7cac4c97e434c3ba54e0223bbaa6fe9bc2321
Jerome Decinco <57111980+CodeKaito@users.noreply.github.com>
2023-01-07 18:59:15 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -11,26 +11,39 @@ import Testimonials from './components/testimonials/Testimonials';
 import Contact from './components/contact/Contact';
 import Footer from './components/footer/Footer';
 import ScrollUp from './components/scrollup/ScrollUp';
+import { useState, useEffect } from 'react';
 
 const App = () => {
+  const [loading, setLoading] = useState(false)
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 3000)
+  }, [])
   return (
-    <>
-    <Header/>
-
-    <main className='main'>
-      <Home />
-      <About />
-      <Skills />
-      <Services />
-      <Qualification />
-      <Testimonials />
-      <Contact />
-      
-    </main>
-
-    <Footer />
-    <ScrollUp />
     
+    <>
+    {
+      loading ?
+      <div className='loading__container'>
+      <svg viewBox="0 0 50 50" className="loading__svg">
+        <circle cx="25" cy="25" r="20" />
+      </svg>
+      </div>
+      :
+    
+    <><Header /><main className='main'>
+            <Home />
+            <About />
+            <Skills />
+            <Services />
+            <Qualification />
+            <Testimonials />
+            <Contact />
+
+          </main><Footer /><ScrollUp /></>
+}
     </>
   )
 }
